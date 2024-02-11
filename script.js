78d73a0f3ad56e15e2b49b781a99e6ebdd9e6a82
uttamkn <uttamkn15@gmail.com>
2024-02-11 21:14:45 +0530

    Add confirm password validation

---
+++ script.js
@@ -0,0 +1,12 @@
+function validateConfirmPassword() {
+    const password = document.getElementById('password');
+    const confirmPassword = document.getElementById('confirmPassword');
+  
+    if (password.value !== confirmPassword.value) {
+      confirmPassword.setCustomValidity('Passwords do not match');
+    } else {
+      confirmPassword.setCustomValidity('');
+    }
+  }
+  
+  
